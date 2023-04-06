35277603b006b5678462b6688f3e42ba8ff3575e
Michael Davies <davies@cs.wisc.edu>
2023-04-06 13:10:06 -0500

    Honestly just need to sync some files

--- blockgemm_tput/bgemm_tput.cu
+++ blockgemm_tput/bgemm_tput.cu
@@ -1,4 +1,4 @@
-#include "blockgemm.cuh"
+#include "warpgemm.cuh"
 #include "utils.cuh"
 
 #define NI 100000
@@ -10,30 +10,29 @@
 // using WarpShape = cutlass::gemm::GemmShape<32, 16, 64>;
 
 using Block = cutlass::gemm::GemmShape<M, N, K>;
+// using BlockGemmOp = ThreadBlockGemmTensorOp<NW, Block>;
 
-using BlockGemmOp = ThreadBlockGemmTensorOp<NW, Block>;
+// __global__ void kernel() {
+//     BlockGemmOp gemm_op;
+//     __shared__ half I[M * K];
+//     __shared__ half W[N * K];
+//     __shared__ half O[M * N];
 
-__global__ void kernel() {
-    BlockGemmOp gemm_op;
-    __shared__ half I[M * K];
-    __shared__ half W[N * K];
-    __shared__ half O[M * N];
+//     int warp_id = threadIdx.y;
+//     int lane_id = threadIdx.x;
 
-    int warp_id = threadIdx.y;
-    int lane_id = threadIdx.x;
+//     for (int i = 0; i < NI; i++) {
+//         gemm_op(
+//             (cutlass::half_t *)I,
+//             (cutlass::half_t *)W,
+//             (cutlass::half_t *)O,
+//             warp_id,
+//             lane_id);
+//     }
 
-    for (int i = 0; i < NI; i++) {
-        gemm_op(
-            (cutlass::half_t *)I,
-            (cutlass::half_t *)W,
-            (cutlass::half_t *)O,
-            warp_id,
-            lane_id);
-    }
+// }
 
-}
 using WarpShape = cutlass::gemm::GemmShape<M, N / NW, K>;
-
 using WarpGemmOp = cutlass::gemm::warp::GemmTensorOp<
     WarpShape,
     cutlass::layout::RowMajorTensorOpMultiplicandCrosswise<
@@ -48,7 +47,7 @@ __global__ void kernel2() {
     WarpGemmOp gemm_op;
     __shared__ half I[M * K];
     __shared__ half W[N * K / NW];
-    __shared__ half O[M * N];
+    __shared__ half O[M * N / NW];
 
     int warp_id = threadIdx.y;
     int lane_id = threadIdx.x;
@@ -72,32 +71,32 @@ int main() {
     // cudaMalloc(&O, M * N * sizeof(half));
 
 
-    printf("Running...\n");
-    float time_ms = cuda_time_kernel_ms(
-        [&]() {
-            kernel<<<grid, block>>>();
-        }
-    );
-
-    printf("gemm1 took %fms\n", time_ms);
-
-    float flops_v1 = 2.0f * M * N * K * NI;
-    float gflops_v1 = flops_v1 / (time_ms * 1e6);
-    printf("+ GFLOPS: %f\n", gflops_v1);
-
-
     // printf("Running...\n");
-    // float time_ms2 = cuda_time_kernel_ms(
+    // float time_ms = cuda_time_kernel_ms(
     //     [&]() {
-    //         kernel2<<<grid, block>>>();
+    //         kernel<<<grid, block>>>();
     //     }
     // );
 
-    // printf("gemm2 took %fms\n", time_ms);
+    // printf("gemm1 took %fms\n", time_ms);
 
-    // float flops_v2 = 2.0f * M * N * K * NI;
-    // float gflops_v2 = flops_v2 / (time_ms2 * 1e6);
-    // printf("+ GFLOPS: %f\n", gflops_v2);
+    // float flops_v1 = 2.0f * M * N * K * NI;
+    // float gflops_v1 = flops_v1 / (time_ms * 1e6);
+    // printf("+ GFLOPS: %f\n", gflops_v1);
+
+
+    printf("Running...\n");
+    float time_ms2 = cuda_time_kernel_ms(
+        [&]() {
+            kernel2<<<grid, block>>>();
+        }
+    );
+
+    printf("gemm2 took %fms\n", time_ms2);
+
+    float flops_v2 = 2.0f * M * N * K * NI;
+    float gflops_v2 = flops_v2 / (time_ms2 * 1e6);
+    printf("+ GFLOPS: %f\n", gflops_v2);
 
     return 0;
 }
