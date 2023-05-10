eea3cb8b0ff3a8ee10c29197ca1857ea83f67de0
Michael Davies <davies@cs.wisc.edu>
2023-05-10 16:27:23 -0500

    Beef up torch test to validate layernorm

--- misc_tests/layer_norm/torch_val.py
+++ misc_tests/layer_norm/torch_val.py
@@ -24,20 +24,65 @@ else:
     print('CUDA not available, layer_norm_cuda will not be available')
 
 def test_layer_norm_128():
-    x = torch.randn(128, 128, dtype=torch.float16, device='cuda')
+    x = torch.randn(64, 128, dtype=torch.float16, device='cuda')
 
     gamma = torch.ones(128, dtype=torch.float16, device='cuda')
     beta = torch.zeros(128, dtype=torch.float16, device='cuda')
 
     y_ref = torch.nn.functional.layer_norm(x, [128], weight=gamma, bias=beta)
     y_act = layer_norm_cuda.layer_norm_128(x, gamma, beta)
+    torch.cuda.synchronize()
 
     print(y_ref)
     print(y_act)
 
     print('L2 error: ', torch.norm(y_ref - y_act))
+    print('Max error: ', torch.max(torch.abs(y_ref - y_act)))
+
+    # for i in range(128*1024):
+    #     max_diff = torch.max(torch.abs(y_ref[i] - y_act[i])).item()
+    #     if max_diff > 0.5:
+    #         print(i, y_ref[i] - y_act[i])
+
+
+def bench_torch_ln(NI=1000):
+    x = torch.randn(128*1024, 128, dtype=torch.float16, device='cuda')
+
+    gamma = torch.ones(128, dtype=torch.float16, device='cuda')
+    beta = torch.zeros(128, dtype=torch.float16, device='cuda')
+
+    t0 = time.perf_counter()
+    for i in range(NI):
+        y_ref = torch.nn.functional.layer_norm(
+            x, [128], weight=gamma, bias=beta)
+
+    torch.cuda.synchronize()
+    t1 = time.perf_counter()
+
+    tt = t1 - t0
+    print(f'Torch layer norm: {NI * 128 / tt} rows/sec ({1e3 * tt / NI:.3f} ms/iter)')
+
+
+def bench_custom_ln(NI=1000):
+    x = torch.randn(128*1024, 128, dtype=torch.float16, device='cuda')
+
+    gamma = torch.ones(128, dtype=torch.float16, device='cuda')
+    beta = torch.zeros(128, dtype=torch.float16, device='cuda')
+
+    t0 = time.perf_counter()
+    for i in range(NI):
+        y_ref = layer_norm_cuda.layer_norm_128(x, gamma, beta)
+
+    torch.cuda.synchronize()
+    t1 = time.perf_counter()
+
+    tt = t1 - t0
+    print(f'Custom layer norm: {NI * 128 / tt} rows/sec ({1e3 * tt / NI:.3f} ms/iter)')
+
 
 if __name__ == '__main__':
     test_layer_norm_128()
+    bench_torch_ln()
+    bench_custom_ln()
 
 
