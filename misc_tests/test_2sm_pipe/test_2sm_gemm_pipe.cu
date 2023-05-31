a2bf02ea447bad36a7d05fa2a087cfa619c1881a
Michael Davies <davies@cs.wisc.edu>
2023-05-31 16:11:56 -0500

    update test_2sm_gemm_pipe

--- misc_tests/test_2sm_pipe/test_2sm_gemm_pipe.cu
+++ misc_tests/test_2sm_pipe/test_2sm_gemm_pipe.cu
@@ -9,8 +9,10 @@
 
 #include <cuda_fp16.h>
 
+#include "pipes.cuh"
 #include "mpmcq.cuh"
 #include "utils.cuh"
+#include "cpasync.cuh"
 
 typedef unsigned long size_t;
 
@@ -18,27 +20,25 @@ using SharedState = cuda::pipeline_shared_state<cuda::thread_scope_block, 2>;
 
 template<typename DT, size_t M, size_t D>
 struct QueueEntry2D {
-    DT buf[M][D];
+    using Element = DT;
+    Element buf[M][D];
 
-    __device__ QueueEntry2D() : buf{(DT)0.0f} {}
+    __device__ half * as_ptr() { return (half *)buf; }
+    __device__ TensorView as_view() { return {as_ptr(), D}; }
+    __device__ QueueEntry2D() {}
 };
 
-template<typename DT, size_t M, size_t D>
-struct SmemBuffers {
-    DT buf[2][M][D];
-
-};
 
 
 struct Pipeline {
     static const size_t d = 128;
-    static const size_t mblk = 64;
-    static const size_t qlen = 8;
+    static const size_t m = 128;
+    static const size_t qlen = 2;
 
-    using QEntry = QueueEntry2D<half, mblk, d>;
+    using QEntry = QueueEntry2D<half, m, d>;
     using Queue = MpmcRingQueue<QEntry, qlen, 1, 1>;
 
-    static const size_t smem_bytes = sizeof(SmemBuffers<half, mblk, d>);
+    static const size_t buf_size = sizeof(QEntry);
 
     Queue q;
 };
@@ -49,101 +49,80 @@ __global__ void init_pipe(Pipeline *pipe) {
 }
 
 
-__device__ void sender(void * smem, Pipeline::Queue& q, int num_iters) {
-    q.reset();
-    auto * buf = (SmemBuffers<half, Pipeline::mblk, Pipeline::d> *)smem;
-    auto this_block = cooperative_groups::this_thread_block();
+template<int NWARPS>
+__device__ void sender(Pipeline::Queue& q, half * in, int num_iters) {
+    MemoryReader ir(in, 0, Pipeline::d);
+    QueueWriter ow(q);
 
-    auto tx = [&](ssize_t seq_n) {
-        auto& slot = q.write_wait(seq_n);
-        cooperative_groups::memcpy_async(
-            this_block,
-            (void *)&slot.data.buf[0][0],
-            (void *)&buf->buf[seq_n % 2][0][0],
-            Pipeline::mblk * Pipeline::d * sizeof(half)
-        );
-    };
+    const int tid = threadIdx.y * 32 + threadIdx.x;
 
-    for (int seq_n = 0; seq_n < num_iters; seq_n++) {
-        tx(seq_n);
-        this_block.sync();
-        q.write_commit(seq_n);
+    for (int i = 0; i < num_iters; i++) {
+        TensorView t_in = ir.read_acquire();
+        TensorView t_out = ow.write_acquire();
+
+        memcpy_sync_1w<NWARPS * 32, Pipeline::buf_size>(t_out.data, t_in.data, tid);
+
+        ir.read_release();
+        ow.write_release();
     }
-
 }
 
-__device__ void receiver(void * smem, Pipeline::Queue& q, int num_iters) {
-    auto * buf = (SmemBuffers<half, Pipeline::mblk, Pipeline::d> *)smem;
-    auto this_block = cooperative_groups::this_thread_block();
+template<int NWARPS>
+__device__ void receiver(Pipeline::Queue& q, half * out, int num_iters) {
+    QueueReader ir(q);
+    MemoryWriter ow(out, 0, Pipeline::d);
 
-    auto rx = [&](ssize_t seq_n) {
-        auto& slot = q.read_wait(seq_n);
-        cooperative_groups::memcpy_async(
-            this_block,
-            (void *)&buf->buf[seq_n % 2][0][0],
-            (void *)&slot.data.buf[0][0],
-            Pipeline::mblk * Pipeline::d * sizeof(half)
-        );
-    };
+    const int tid = threadIdx.y * 32 + threadIdx.x;
 
-    for (int seq_n = 0; seq_n < num_iters; seq_n++) {
-        rx(seq_n);
-        this_block.sync();
-        q.read_commit(seq_n);
+    for (int i = 0; i < num_iters; i++) {
+        TensorView t_in = ir.read_acquire();
+        TensorView t_out = ow.write_acquire();
+
+        memcpy_sync_1w<NWARPS * 32, Pipeline::buf_size>(t_out.data, t_in.data, tid);
+
+        ir.read_release();
+        ow.write_release();
     }
 }
 
 
 
-__global__ void kernel(Pipeline *pipe, int num_iters) {
+template<int NWARPS>
+__global__ void kernel(Pipeline *pipe, half * in, half * out, int num_iters) {
     int bid = blockIdx.x;
-    extern __shared__ void * smem[];
 
     if (bid == 0) {
-        sender(smem, pipe->q, num_iters);
+        sender<NWARPS>(pipe->q, in, num_iters);
     } else {
-        receiver(smem, pipe->q, num_iters);
+        receiver<NWARPS>(pipe->q, out, num_iters);
     }
 
 }
 
 int main() {
-    const int num_iters = 1000000;
-
-    cudaErrCheck(cudaDeviceSetCacheConfig(cudaFuncCachePreferShared));
-    cudaErrCheck(cudaFuncSetAttribute(
-        kernel,
-        cudaFuncAttributePreferredSharedMemoryCarveout,
-        cudaSharedmemCarveoutMaxShared));
-
-    cudaErrCheck(cudaFuncSetAttribute(
-        kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, Pipeline::smem_bytes));
-
+    const int num_iters = 1000;
 
     Pipeline * pipe;
     cudaErrCheck(cudaMallocManaged(&pipe, sizeof(Pipeline)));
 
+    half * in;
+    half * out;
 
-    printf("Init...\n");
-    init_pipe<<<1, 1>>>(pipe);
-    cudaErrCheck(cudaDeviceSynchronize());
+    cudaErrCheck(cudaMallocManaged(&in, Pipeline::buf_size));
+    cudaErrCheck(cudaMallocManaged(&out, Pipeline::buf_size));
+
 
     dim3 block(32, 8);
-
-    printf("SMEM: %lu\n", Pipeline::smem_bytes);
 
     printf("Running...\n");
     float time_ms = cuda_time_kernel_ms(
         [&]() {
-            kernel<<<2, block, Pipeline::smem_bytes>>>(pipe, num_iters);
+            kernel<8><<<2, block>>>(pipe, in, out, num_iters);
         }
     );
 
     printf("Time: %f ms\n", time_ms);
 
-    float num_bytes = num_iters * Pipeline::mblk * Pipeline::d * sizeof(half);
-    float bw = num_bytes / (time_ms * 1e-3) / (1024.0f * 1024.0f * 1024.0f);
-    printf("BW: %f GB/s\n", bw);
 
     return 0;
 }
