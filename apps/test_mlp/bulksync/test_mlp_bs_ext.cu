bea13e1810d653331ccb7c1f83214e81e80cc9e7
Michael Davies <davies@cs.wisc.edu>
2023-06-06 14:52:44 -0500

    Add bulksync_gemm and bulksync test_mlp

---
+++ apps/test_mlp/bulksync/test_mlp_bs_ext.cu
@@ -0,0 +1,89 @@
+
+
+#include <torch/extension.h>
+#include <ATen/ATen.h>
+
+#include <cuda_fp16.h>
+
+#include "bulksync_gemm.cuh"
+#include "utils.cuh"
+
+#define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+
+
+const size_t MM = 1280 * 1024;
+
+
+void testmlp_out(
+    at::Tensor x,     // [M, 128]
+    at::Tensor w1,    // [128, 128]
+    at::Tensor b1,    // [128]
+    at::Tensor w2,    // [128, 128]
+    at::Tensor b2,    // [128]
+    at::Tensor w3,    // [128, 128]
+    at::Tensor b3,    // [128]
+    at::Tensor y1,    // [M, 128]
+    at::Tensor y2,    // [M, 128]
+    at::Tensor out    // [M, 128]
+) {
+    CHECK_INPUT(x);
+    CHECK_INPUT(w1);
+    CHECK_INPUT(b1);
+    CHECK_INPUT(w2);
+    CHECK_INPUT(b2);
+    CHECK_INPUT(w3);
+    CHECK_INPUT(b3);
+
+    assert(x.size(0) == MM);
+    assert(x.size(1) == 128);
+    assert(w1.size(0) == 128 && w1.size(1) == 128 && b1.size(0) == 128);
+    assert(w2.size(0) == 128 && w2.size(1) == 128 && b2.size(0) == 128);
+    assert(w3.size(0) == 128 && w3.size(1) == 128 && b3.size(0) == 128);
+
+
+    bulksync_gemm<MM, 128, 128>(
+        (half *)x.data_ptr<at::Half>(),
+        (half *)w1.data_ptr<at::Half>(),
+        (half *)b1.data_ptr<at::Half>(),
+        (half *)y1.data_ptr<at::Half>()
+    );
+
+    bulksync_gemm<MM, 128, 128>(
+        (half *)y1.data_ptr<at::Half>(),
+        (half *)w2.data_ptr<at::Half>(),
+        (half *)b2.data_ptr<at::Half>(),
+        (half *)y2.data_ptr<at::Half>()
+    );
+
+    bulksync_gemm<MM, 128, 128>(
+        (half *)y2.data_ptr<at::Half>(),
+        (half *)w3.data_ptr<at::Half>(),
+        (half *)b3.data_ptr<at::Half>(),
+        (half *)out.data_ptr<at::Half>()
+    );
+}
+
+at::Tensor testmlp(
+    at::Tensor x,     // [M, 384]
+    at::Tensor w1,    // [384, 128]
+    at::Tensor b1,    // [128]
+    at::Tensor w2,    // [128, 128]
+    at::Tensor b2,    // [128]
+    at::Tensor w3,    // [128, 128]
+    at::Tensor b3     // [128]
+) {
+
+    at::Tensor y1 = at::zeros({x.size(0), 128}, x.options());
+    at::Tensor y2 = at::zeros({x.size(0), 128}, x.options());
+    at::Tensor out = at::zeros({x.size(0), 128}, x.options());
+    testmlp_out(x, w1, b1, w2, b2, w3, b3, y1, y2, out);
+    return out;
+}
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("testmlp", &testmlp, "testmlp");
+    m.def("testmlp_out", &testmlp_out, "testmlp_out");
+}
+
---
+++ common/bulksync_gemm.cuh
@@ -0,0 +1,98 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include "cutlass/cutlass.h"
+#include "cutlass/gemm/device/gemm.h"
+
+#include "utils.cuh"
+#include "refgemm.cuh"
+
+using ThreadblockShape = cutlass::gemm::GemmShape<128, 128, 32>;
+
+template<size_t MM, size_t NN, size_t KK, bool RELU=false>
+cudaError_t bulksync_gemm(
+    half * x,
+    half * w,
+    half * b,
+    half * y
+) {
+    using RowMajor = cutlass::layout::RowMajor;
+
+    cutlass::Status status;
+
+    if (RELU) {
+        using CutlassGemm = cutlass::gemm::device::Gemm<
+            cutlass::half_t, RowMajor,
+            cutlass::half_t, RowMajor,
+            cutlass::half_t, RowMajor,
+            cutlass::half_t,
+            cutlass::arch::OpClassWmmaTensorOp,
+            cutlass::arch::Sm80,
+            cutlass::gemm::GemmShape<128, 128, 32>,
+            cutlass::gemm::GemmShape<64, 64, 32>,
+            cutlass::gemm::GemmShape<16, 16, 16>,
+            cutlass::epilogue::thread::LinearCombinationRelu<
+                cutlass::half_t,
+                128 / cutlass::sizeof_bits<cutlass::half_t>::value,
+                cutlass::half_t,
+                cutlass::half_t,
+                cutlass::epilogue::thread::ScaleType::NoBetaScaling
+            >
+        >;
+
+        CutlassGemm gemm_operator;
+
+        CutlassGemm::Arguments args(
+            {(int)MM, (int)NN, (int)KK},
+            {(cutlass::half_t *)x, (int)KK},
+            {(cutlass::half_t *)w, (int)NN},
+            {(cutlass::half_t *)b, (int)0},
+            {(cutlass::half_t *)y, (int)NN},
+            {cutlass::half_t(1.0f), cutlass::half_t(0.0f), cutlass::half_t(0.0f)}
+
+        );
+
+        status = gemm_operator(args);
+    }
+    else {
+
+        using CutlassGemm = cutlass::gemm::device::Gemm<
+            cutlass::half_t, RowMajor,
+            cutlass::half_t, RowMajor,
+            cutlass::half_t, RowMajor,
+            cutlass::half_t,
+            cutlass::arch::OpClassWmmaTensorOp,
+            cutlass::arch::Sm80,
+            cutlass::gemm::GemmShape<128, 128, 32>,
+            cutlass::gemm::GemmShape<64, 64, 32>,
+            cutlass::gemm::GemmShape<16, 16, 16>,
+            cutlass::epilogue::thread::LinearCombination<
+                cutlass::half_t,
+                128 / cutlass::sizeof_bits<cutlass::half_t>::value,
+                cutlass::half_t,
+                cutlass::half_t
+            >
+        >;
+
+        CutlassGemm gemm_operator;
+
+        CutlassGemm::Arguments args(
+            {(int)MM, (int)NN, (int)KK},
+            {(cutlass::half_t *)x, (int)KK},
+            {(cutlass::half_t *)w, (int)NN},
+            {(cutlass::half_t *)b, (int)0},
+            {(cutlass::half_t *)y, (int)NN},
+            {cutlass::half_t(1.0f), cutlass::half_t(0.0f)}
+        );
+
+        status = gemm_operator(args);
+    }
+
+    if (status != cutlass::Status::kSuccess) {
+        return cudaErrorUnknown;
+    }
+
+    return cudaSuccess;
+}
