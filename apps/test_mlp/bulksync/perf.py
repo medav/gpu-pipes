ad5392bbee2c88b37956eb0c12b976d3a7588263
Michael Davies <davies@cs.wisc.edu>
2023-06-06 14:58:57 -0500

    Extract out common benchmark code

--- apps/test_mlp/bulksync/perf.py
+++ apps/test_mlp/bulksync/perf.py
@@ -1,43 +1,34 @@
 import os
 import time
 import torch
+from common import utils
 from . import ext
 
 def test_performance(NI=1000):
     M = 1280 * 1024
-    print('======== Performance ========')
     torch.manual_seed(0)
     x = torch.randn(M, 128, dtype=torch.float16, device='cuda')
 
     w1 = torch.randn(128, 128, dtype=torch.float16, device='cuda')
-    b1 = torch.randn(128, dtype=torch.float16, device='cuda')
+    b1 = torch.randn(128,      dtype=torch.float16, device='cuda')
     w2 = torch.randn(128, 128, dtype=torch.float16, device='cuda')
-    b2 = torch.randn(128, dtype=torch.float16, device='cuda')
+    b2 = torch.randn(128,      dtype=torch.float16, device='cuda')
     w3 = torch.randn(128, 128, dtype=torch.float16, device='cuda')
-    b3 = torch.randn(128, dtype=torch.float16, device='cuda')
-
+    b3 = torch.randn(128,      dtype=torch.float16, device='cuda')
     y1 = torch.zeros(M, 128, dtype=torch.float16, device='cuda')
     y2 = torch.zeros(M, 128, dtype=torch.float16, device='cuda')
     out = torch.zeros(M, 128, dtype=torch.float16, device='cuda')
 
-    t0 = time.perf_counter()
-    for _ in range(NI):
-        ext.testmlp_cuda.testmlp_out(
-            x,
-            w1, b1,
-            w2, b2,
-            w3, b3,
-            y1,
-            y2,
-            out
-        )
-    torch.cuda.synchronize()
-    t1 = time.perf_counter()
+    flops = M * (128 * 128 + 128 * 128 + 128 * 128) * 2
 
-    tt = t1 - t0
-    flops = M * (128 * 128 + 128 * 128 + 128 * 128) * 2 * NI
-    print(f'Full MLP: {tt:.3f} s, {flops / tt / 1e9:.3f} GFLOPS')
-
+    utils.benchmark(
+        ext.testmlp_cuda.testmlp_out,
+        x,
+        w1, b1,
+        w2, b2,
+        w3, b3,
+        y1, y2, out,
+        flops=flops)
 
 
 if __name__ == '__main__': test_performance()
