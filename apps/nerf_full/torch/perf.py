e33184c7192e8ae69232fed26b0db7c23200737c
Michael Davies <davies@cs.wisc.edu>
2023-06-12 16:07:57 -0500

    Add nerf torch version

---
+++ apps/nerf_full/torch/perf.py
@@ -0,0 +1,31 @@
+import os
+import time
+import torch
+from common import utils
+from . import model
+
+def test_performance(NI=1000):
+    M = 65536
+    torch.manual_seed(0)
+    net = model.Nerf(60, 24).eval().half().cuda()
+    x = torch.randn(M, 60, dtype=torch.float16, device='cuda')
+    d = torch.randn(M, 24, dtype=torch.float16, device='cuda')
+
+    flops = M * (
+        256 * 60 +
+        256 * 256 +
+        256 * 256 +
+        256 * 256 +
+        256 * 256 +
+        256 * 316 +
+        256 * 256 +
+        256 * 256 +
+        256 * 256 +
+        256 * 280 +
+        3 * 256 +
+        1 * 256
+    )
+
+    utils.benchmark(net, x, d, flops=flops)
+
+if __name__ == '__main__': test_performance()
