de1b18cc913df5d78053f159546e836a5ba9ea57
Michael Davies <davies@cs.wisc.edu>
2023-06-19 12:32:11 -0500

    Optimize pipe_concat

--- common/pipe_concat.cuh
+++ common/pipe_concat.cuh
@@ -1,8 +1,42 @@
 #pragma once
 #include "cpasync.cuh"
 
-#include "concat.cuh"
 #include "common.cuh"
+
+template<typename T, int N0, int N1>
+__device__ void concat2_internal(
+    T * a,
+    T * b,
+    T * c,
+    const int M
+) {
+    static_assert(N0 * sizeof(T) % 16 == 0, "N0 must be a multiple of 16");
+    static_assert(N1 * sizeof(T) % 16 == 0, "N1 must be a multiple of 16");
+
+    constexpr int N0bytes = N0 * sizeof(T);
+    constexpr int N1bytes = N1 * sizeof(T);
+
+    constexpr int N0_4 = N0bytes / 16;
+    constexpr int N1_4 = N1bytes / 16;
+    constexpr int NO_4 = (N0bytes + N1bytes) / 16;
+
+    const int lane = threadIdx.x;
+    const int warp = threadIdx.y;
+
+    int4 * a4 = reinterpret_cast<int4 *>(a);
+    int4 * b4 = reinterpret_cast<int4 *>(b);
+    int4 * c4 = reinterpret_cast<int4 *>(c);
+
+    for (int i = lane; i < M; i += blockDim.x) {
+        for (int d = warp; d < NO_4; d += blockDim.y) {
+            if (d < N0_4) {
+                c4[i * NO_4 + d] = a4[i * N0_4 + d];
+            } else {
+                c4[i * NO_4 + d] = b4[i * N1_4 + d - N0_4];
+            }
+        }
+    }
+}
 
 template<int M, int N0, int N1>
 struct ConcatShape {
@@ -27,13 +61,11 @@ __device__ void pipe_concat(
         TensorView it1 = ir1.read_acquire();
         TensorView ot = ow.write_acquire();
 
-        concat2_internal<half>(
+        concat2_internal<half, Shape::kN0, Shape::kN1>(
             it0.data,
             it1.data,
             ot.data,
-            Shape::kM,
-            Shape::kN0,
-            Shape::kN1);
+            Shape::kM);
 
         ir0.read_release();
         ir1.read_release();
