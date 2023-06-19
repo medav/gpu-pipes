ed3d44d3e3744d89c1c2c6773bfa73f73250d39c
Michael Davies <davies@cs.wisc.edu>
2023-06-19 00:11:51 -0500

    Fix nerf_a bulksync standalone

--- apps/nerf_a/bulksync/nerf_a_bs_cc.cu
+++ apps/nerf_a/bulksync/nerf_a_bs_cc.cu
@@ -2,7 +2,8 @@
 
 #include <cuda_fp16.h>
 
-#include "test_mlp_bs.cuh"
+#include "wrapper_utils.cuh"
+#include "nerf_a_bs.cuh"
 #include "bulksync_gemm.cuh"
 #include "utils.cuh"
 
@@ -10,51 +11,53 @@ int main(int argc, char * argv[]) {
     const size_t NI = argc > 1 ? std::atoi(argv[1]) : 1000;
     printf("NI: %zu\n", NI);
 
-    half * x_dev = nullptr;
-    half * w1_dev = nullptr;
-    half * b1_dev = nullptr;
-    half * w2_dev = nullptr;
-    half * b2_dev = nullptr;
-    half * w3_dev = nullptr;
-    half * b3_dev = nullptr;
-    half * y1_dev = nullptr;
-    half * y2_dev = nullptr;
-    half * out_dev = nullptr;
+    ALLOC_TENSOR_2D(x, MM, 64)
+    ALLOC_TENSOR_2D(d, MM, 32)
 
-    cudaErrCheck(cudaMalloc(&x_dev, MM * DD * sizeof(*x_dev)));
-    cudaErrCheck(cudaMalloc(&w1_dev, DD * DD * sizeof(*x_dev)));
-    cudaErrCheck(cudaMalloc(&b1_dev, DD * sizeof(*x_dev)));
-    cudaErrCheck(cudaMalloc(&w2_dev, DD * DD * sizeof(*x_dev)));
-    cudaErrCheck(cudaMalloc(&b2_dev, DD * sizeof(*x_dev)));
-    cudaErrCheck(cudaMalloc(&w3_dev, DD * DD * sizeof(*x_dev)));
-    cudaErrCheck(cudaMalloc(&b3_dev, DD * sizeof(*x_dev)));
-    cudaErrCheck(cudaMalloc(&y1_dev, MM * DD * sizeof(*x_dev)));
-    cudaErrCheck(cudaMalloc(&y2_dev, MM * DD * sizeof(*x_dev)));
-    cudaErrCheck(cudaMalloc(&out_dev, MM * DD * sizeof(*x_dev)));
+    ALLOC_LINEAR_WEIGHTS(l1, 64, 256)
+    ALLOC_LINEAR_WEIGHTS(l2, 256, 256)
+    ALLOC_LINEAR_WEIGHTS(l3, 256, 256)
+    ALLOC_LINEAR_WEIGHTS(l4, 256, 256)
+    ALLOC_LINEAR_WEIGHTS(l5, 256, 256)
+    ALLOC_LINEAR_WEIGHTS(l6, 256, 256)
+    ALLOC_LINEAR_WEIGHTS(l7, 256, 256)
+    ALLOC_LINEAR_WEIGHTS(l8, 256, 256)
+
+    ALLOC_TENSOR_2D(t1, MM, 320)
+    ALLOC_TENSOR_2D(t2, MM, 320)
+    ALLOC_TENSOR_2D(out, MM, 256)
 
 
     float time_ms = cuda_time_kernel_ms([&]() {
         for (size_t i = 0; i < NI; i++) {
-            testmlp_bs<MM, DD>(
-                x_dev,
-                w1_dev,
-                b1_dev,
-                w2_dev,
-                b2_dev,
-                w3_dev,
-                b3_dev,
-                y1_dev,
-                y2_dev,
-                out_dev);
+            nerf_a_bs<MM>(
+                x,
+                l1_w, l1_b,
+                l2_w, l2_b,
+                l3_w, l3_b,
+                l4_w, l4_b,
+                l5_w, l5_b,
+                l6_w, l6_b,
+                l7_w, l7_b,
+                l8_w, l8_b,
+                t1, t2,
+                out
+            );
         }
     });
 
     printf("Avg latency: %f ms\n", time_ms / (float)NI);
 
     const float flops = MM * (
-        DD * DD +
-        DD * DD +
-        DD * DD) * 2.0f;
+        256 * 60 +
+        256 * 256 +
+        256 * 256 +
+        256 * 256 +
+        256 * 256 +
+        256 * 316 +
+        256 * 256 +
+        256 * 256
+    ) * 2.0f;
 
     printf("GFLOPS: %f\n", NI * flops / (time_ms * 1e-3f) / 1e9f);
 }
