2cac23a1577926dd0afc0630f490908e72bd5225
Michael Davies <davies@cs.wisc.edu>
2023-06-19 00:22:37 -0500

    Fix standalone launchers for gc_nodes and gc_edges

--- apps/gc_edges/bulksync/gc_edges_bs_cc.cu
+++ apps/gc_edges/bulksync/gc_edges_bs_cc.cu
@@ -2,6 +2,7 @@
 
 #include <cuda_fp16.h>
 
+#include "wrapper_utils.cuh"
 #include "gc_edges_bs.cuh"
 #include "bulksync_gemm.cuh"
 #include "utils.cuh"
@@ -10,45 +11,29 @@ int main(int argc, char * argv[]) {
     const size_t NI = argc > 1 ? std::atoi(argv[1]) : 1000;
     printf("NI: %zu\n", NI);
 
-    half * x_dev = nullptr;
-    half * w1_dev = nullptr;
-    half * b1_dev = nullptr;
-    half * w2_dev = nullptr;
-    half * b2_dev = nullptr;
-    half * ga_dev = nullptr;
-    half * be_dev = nullptr;
-    half * t1_dev = nullptr;
-    half * t2_dev = nullptr;
-    half * t3_dev = nullptr;
-    half * out_dev = nullptr;
-
-    cudaErrCheck(cudaMalloc(&x_dev, MM * 128 * sizeof(*x_dev)));
-    cudaErrCheck(cudaMalloc(&w1_dev, 128 * 512 * sizeof(*w1_dev)));
-    cudaErrCheck(cudaMalloc(&b1_dev, 512 * sizeof(*b1_dev)));
-    cudaErrCheck(cudaMalloc(&w2_dev, 512 * 128 * sizeof(*w2_dev)));
-    cudaErrCheck(cudaMalloc(&b2_dev, 128 * sizeof(*b2_dev)));
-    cudaErrCheck(cudaMalloc(&ga_dev, 128 * sizeof(*ga_dev)));
-    cudaErrCheck(cudaMalloc(&be_dev, 128 * sizeof(*be_dev)));
-    cudaErrCheck(cudaMalloc(&t1_dev, MM * 512 * sizeof(*t1_dev)));
-    cudaErrCheck(cudaMalloc(&t2_dev, MM * 128 * sizeof(*t2_dev)));
-    cudaErrCheck(cudaMalloc(&t3_dev, MM * 128 * sizeof(*t3_dev)));
-    cudaErrCheck(cudaMalloc(&out_dev, MM * 128 * sizeof(*out_dev)));
+    ALLOC_TENSOR_2D(x, MM, 1536)
+    ALLOC_LINEAR_WEIGHTS(l0, 1536, 512)
+    ALLOC_LINEAR_WEIGHTS(l1, 512, 512)
+    ALLOC_LN_WEIGHTS(ln0, 512)
+    ALLOC_TENSOR_2D(t0, MM, 512)
+    ALLOC_TENSOR_2D(t1, MM, 512)
+    ALLOC_TENSOR_2D(out, MM, 512)
 
     float time_ms = cuda_time_kernel_ms([&]() {
         for (size_t i = 0; i < NI; i++) {
             gc_edges_bs<MM>(
-                x_dev,
-                w1_dev, b1_dev,
-                w2_dev, b2_dev,
-                ga_dev, be_dev,
-                t1_dev, t2_dev, t3_dev,
-                out_dev);
+                x,
+                l0_w, l0_b,
+                l1_w, l1_b,
+                ln0_ga, ln0_be,
+                t0, t1, out
+            );
         }
     });
 
     printf("Avg latency: %f ms\n", time_ms / (float)NI);
 
-    const float flops = MM * (128 * 512 + 512 * 128) * 2.0f;
+    const float flops = MM * (1536 * 512 + 512 * 512) * 2.0f;
 
     printf("GFLOPS: %f\n", NI * flops / (time_ms * 1e-3f) / 1e9f);
 }
--- apps/gc_edges/pipelined/gc_edges_pl_cc.cu
+++ apps/gc_edges/pipelined/gc_edges_pl_cc.cu
@@ -2,7 +2,8 @@
 
 #include <cuda_fp16.h>
 
-#include "bert_ffn_pl.cuh"
+#include "wrapper_utils.cuh"
+#include "gc_edges_pl.cuh"
 #include "bulksync_gemm.cuh"
 #include "utils.cuh"
 
@@ -13,39 +14,27 @@ int main(int argc, char * argv[]) {
     printf("NI: %zu\n", NI);
 
 
-    half * x_dev = nullptr;
-    half * w1_dev = nullptr;
-    half * b1_dev = nullptr;
-    half * w2_dev = nullptr;
-    half * b2_dev = nullptr;
-    half * ga_dev = nullptr;
-    half * be_dev = nullptr;
-    half * out_dev = nullptr;
-
-    cudaErrCheck(cudaMalloc(&x_dev, MM * 128 * sizeof(*x_dev)));
-    cudaErrCheck(cudaMalloc(&w1_dev, 128 * 512 * sizeof(*w1_dev)));
-    cudaErrCheck(cudaMalloc(&b1_dev, 512 * sizeof(*b1_dev)));
-    cudaErrCheck(cudaMalloc(&w2_dev, 512 * 128 * sizeof(*w2_dev)));
-    cudaErrCheck(cudaMalloc(&b2_dev, 128 * sizeof(*b2_dev)));
-    cudaErrCheck(cudaMalloc(&ga_dev, 128 * sizeof(*ga_dev)));
-    cudaErrCheck(cudaMalloc(&be_dev, 128 * sizeof(*be_dev)));
-    cudaErrCheck(cudaMalloc(&out_dev, MM * 128 * sizeof(*out_dev)));
+    ALLOC_TENSOR_2D(x, MM, 1536)
+    ALLOC_LINEAR_WEIGHTS(l0, 1536, 512)
+    ALLOC_LINEAR_WEIGHTS(l1, 512, 512)
+    ALLOC_LN_WEIGHTS(ln0, 512)
+    ALLOC_TENSOR_2D(out, MM, 512)
 
 
-    dim3 grid(BertFfn::n_cols, BertFfn::n_rows);
+    dim3 grid(GcEdgesMlp::n_cols, GcEdgesMlp::n_rows);
     dim3 block(32, num_warps);
 
     configure_smem_once();
 
     float time_ms = cuda_time_kernel_ms([&]() {
         for (size_t i = 0; i < NI; i++) {
-            bert_ffn_device<<<grid, block, max_smem>>>(
+            gc_edges_device<<<grid, block, max_smem>>>(
                 MM,
-                x_dev,
-                w1_dev, b1_dev,
-                w2_dev, b2_dev,
-                ga_dev, be_dev,
-                out_dev,
+                x,
+                l0_w, l0_b,
+                l1_w, l1_b,
+                ln0_ga, ln0_be,
+                out,
                 global_queue_space()
             );
         }
@@ -53,12 +42,8 @@ int main(int argc, char * argv[]) {
 
     printf("Avg latency: %f ms\n", time_ms / (float)NI);
 
-    const float flops = MM * (
-        128 * 512 +
-        512 * 128
-    ) * 2.0f;
+    const float flops = MM * (1536 * 512 + 512 * 512) * 2.0f;
 
     printf("FLOPS: %f\n", flops);
-
     printf("GFLOPS: %f\n", (float)NI * flops / (time_ms * 1e-3f) / 1e9f);
 }
