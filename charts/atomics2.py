3c6f597de9cf0b2bfd3fda4d03d24d2800a0b7d1
Michael Davies <davies@cs.wisc.edu>
2023-07-11 21:47:09 -0500

    Update charts

---
+++ charts/atomics2.py
@@ -0,0 +1,139 @@
+
+
+# nb, nw, nv
+cont_1 = {
+    (1, 4, 1): 25188126,
+    (2, 4, 2): 50873944,
+    (4, 4, 4): 100995752,
+    (8, 4, 8): 201991552,
+    (16, 4, 16): 403983840,
+    (32, 4, 32): 807973504,
+    (64, 4, 64): 1562897536,
+    (108, 4, 108): 1581495808,
+}
+cont_2 = {
+    (2, 4, 1): 50841896,
+    (4, 4, 2): 100996536,
+    (8, 4, 4): 201992224,
+    (16, 4, 8): 403988960,
+    (32, 4, 16): 504141984,
+    (64, 4, 32): 927001536,
+    (108, 4, 54): 1686451840,
+    (216, 4, 108): 1744399488,
+}
+cont_4 = {
+    (4, 4, 1): 100994760,
+    (8, 4, 2): 202056544,
+    (16, 4, 4): 219619728,
+    (32, 4, 8): 471515840,
+    (64, 4, 16): 556601216,
+    (108, 4, 32): 900235904,
+    (216, 4, 64): 1887217920,
+    (432, 4, 108): 1625763968,
+}
+cont_8 = {
+    (8, 4, 1): 202083360,
+    (16, 4, 2): 236169728,
+    (32, 4, 4): 261581136,
+    (64, 4, 8): 485346848,
+    (108, 4, 16): 547374656,
+    (216, 4, 32): 1012806400,
+    (432, 4, 64): 2057894272,
+    (864, 4, 108): 1621274752,
+}
+cont_16 = {
+    (16, 4, 1): 221200816,
+    (32, 4, 2): 278971680,
+    (64, 4, 4): 263265456,
+    (108, 4, 8): 493591488,
+    (216, 4, 16): 560784512,
+    (432, 4, 32): 1059412288,
+    (864, 4, 64): 1980104576,
+}
+cont_32 = {
+    (32, 4, 1): 269997760,
+    (64, 4, 2): 286684704,
+    (108, 4, 3): 256180080,
+}
+cont_64 = {
+    (64, 4, 1): 269998976,
+    (108, 4, 2): 281262464,
+}
+cont_108 = {
+    (108, 4, 1): 269999264,
+}
+
+def lookup(nblocks : int, nwarps : int, nvars : int) -> float:
+    return data_lookup[(nblocks, nwarps, nvars)]
+
+import numpy as np
+from matplotlib import pyplot as plt
+import itertools
+
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+COL_WIDTH = (8.5 - 1.5 - 0.25) / 2
+TEXT_WIDTH = 8.5 - 1.5
+
+global_nvs = np.array([1, 3, 6, 13, 27, 54, 108])
+global_nbs = np.array([1, 6, 27, 108])
+
+fig, ax = plt.subplots(figsize=(COL_WIDTH, 2.5))
+
+tables = {
+    1: cont_1,
+    2: cont_2,
+    4: cont_4,
+    8: cont_8,
+    16: cont_16,
+    32: cont_32,
+    64: cont_64,
+    108: cont_108,
+}
+
+
+for cont in [1, 2, 4, 16, 64, 108]:
+    keys = np.array([
+        [nb, nw, nv]
+        for (nb, nw, nv) in tables[cont].keys()
+    ])
+
+    idxs = np.argsort(keys[:, 2], axis=0)
+    keys = keys[idxs]
+
+    x_points = keys[:, 2]
+
+    y_points = np.array([
+        tables[cont][tuple(k)]
+        for k in keys
+    ]) / keys[:, 0]
+
+    print(x_points)
+    print(y_points)
+
+    ax.plot(x_points, y_points, label=f'{cont}x', marker='o')
+
+plt.semilogx()
+# plt.loglog()
+
+plt.xlabel('# Variables', fontsize=8)
+plt.xticks([1, 2, 4, 8, 16, 32, 64, 108], [1, 2, 4, 8, 16, 32, 64, 108], fontsize=8)
+
+plt.ylabel('atomicAdd / sec / Block', fontsize=8)
+plt.yticks(fontsize=8)
+plt.ylim([0, 3.2e7])
+
+plt.title('Performance of atomicAdd', fontsize=10)
+
+plt.legend(
+    fontsize=6,
+    ncol=6,
+    loc='upper center',
+    # bbox_to_anchor=(0.5, -0.15),
+    markerscale=0.8,
+    columnspacing=1,
+    handlelength=1.5,
+    frameon=False)
+plt.tight_layout()
+plt.savefig('atomics.pdf')
+
+
--- charts/qperf.py
+++ charts/qperf.py
@@ -5,54 +5,60 @@ colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 COL_WIDTH = (8.5 - 1.5 - 0.25) / 2
 TEXT_WIDTH = 8.5 - 1.5
 
-l2bw = np.array([[
-    1.4,
-    2.8,
-    6.5,
-    11.9,
-    22.5,
-    22.5,
-    22.5,
-    22.3
-], [
-    74.0,
-    154.0,
-    302.0,
-    626.0,
-    837.0,
-    828.0,
-    863.0,
-    860.0
-]])
-
-num_prod_cons = np.array([1, 54])
-
-payload_kb = np.array([
-    1,
-    2,
-    4,
-    8,
-    16,
-    32,
-    64,
-    128
+l2bw = np.array([
+    [0.72, 39.94, 8.53, 431.03],
+    [1.46, 72.09, 16.7, 659.64],
+    [2.61, 143.01, 39.69, 969.44],
+    [6.65, 347.10, 32.45, 912.9],
+    [9.88, 527.12, 38.39, 1017.73],
+    [20.56, 1011.64, 37.79, 1336.22],
+    [39.27, 1553.40, 45.06, 1662.11],
+    [42.67, 2005.75, 49.52, 1835.31],
+    [51.87, 2060.69, 53.51, 1855.27],
+    [54.09, 1254.44, 54.8, 1153.8],
+    [55.89, 1248.38, 57.1, 1155.42],
+    [57.15, 1247.80, 57.58, 1191.13]
 ])
 
-fig, ax = plt.subplots(figsize=(COL_WIDTH, 2))
+print(l2bw[2] / l2bw[0])
+
+num_prod_cons = np.array([1, 27])
+
+payload_kb = np.array([
+    1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048
+])
+
+labels = np.array([
+    '1', '2', '4', '8', '16', '32', '64', '128', '256', '512', '1k', '2k'
+])
+
+fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(COL_WIDTH, 3), sharex=True, )
 
 lines = []
 labels = []
 
-for i in range(len(num_prod_cons)):
-    line, = ax.plot(payload_kb, l2bw[i], color=colors[i], label=f'{num_prod_cons[i]} Queues')
-    lines.append(line)
+ax1.plot(payload_kb, l2bw[:, 0], label=f'1 Q w/ sync')
+ax1.plot(payload_kb, l2bw[:, 2], label=f'1 Q NO sync')
+ax1.legend(fontsize=6)
+ax1.set_ylim(0, 60)
+ax1.tick_params(axis='y', labelsize=8)
+# ax1.set_ylabel('Bandwidth (GB/s)', fontsize=8)
 
-plt.loglog()
-plt.legend(fontsize=8, loc='lower right')
+ax2.plot([1, 2048], [1555, 1555], '--', color='black', label='L2 BW')
+ax2.plot(payload_kb, l2bw[:, 1], label=f'54 Q w/ sync')
+ax2.plot(payload_kb, l2bw[:, 3], label=f'54 Q NO sync')
+ax2.legend(fontsize=6)
+ax2.set_ylim(0, 2300)
+ax2.tick_params(axis='y', labelsize=8)
+# fig.set_ylabel('Bandwidth (GB/s)', fontsize=8)
+
+fig.text(0.04, 0.5, 'Bandwidth (GB/s)', va='center', rotation='vertical', fontsize=8)
+
+
+plt.semilogx(base=2)
 plt.xlabel('Payload Size (KB)', fontsize=8)
-plt.xticks(payload_kb, payload_kb, fontsize=8)
-plt.ylabel('Bandwidth (GB/s)', fontsize=8)
-plt.yticks(fontsize=8)
-plt.title('Performance of Ring Queue', fontsize=10)
+
+
 plt.tight_layout()
+plt.subplots_adjust(left=0.2)
 plt.savefig('qperf.pdf')
